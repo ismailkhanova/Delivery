ba5fc7f70ab0d08d88959ce37ed696580451ed5d
Aquinology <riabova_e@iuca.kg>
2020-11-26 17:09:36 +0600

    Categories

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import StoreList, StoreProduct, ProductCategory, ProductDetail
+from .views import StoreList, ProductCategory, ProductDetail, StoreProduct
 
 urlpatterns = [
     path("", StoreList.as_view(), name="store_list"),
-    path("store/<slug>/", StoreProduct.as_view(), name="store_products"),
-    path("cat/<slug>/", ProductCategory.as_view(), name="product_category"),
+    path("store/<slug>/", ProductCategory.as_view(), name="product_category"),
+    path("category/<slug>/", StoreProduct.as_view(), name="cat_products"),
     path("<int:pk>/", ProductDetail.as_view(), name="product_detail"),
     ]
--- products/views.py
+++ products/views.py
@@ -6,29 +6,28 @@ from .models import Store, Product, Category
 class StoreList(ListView):
     model = Store
     template_name = 'products/store_list.html'
-    context_object_name = 'store'
+    context_object_name = 'stores'
+
+
+class ProductCategory(ListView):
+    model = Category
+    template_name = 'products/product_list.html'
+    context_object_name = 'categories'
+
+    def get_queryset(self):
+        self.store = get_object_or_404(Store, slug=self.kwargs['slug'])
+        return Category.objects.filter(store=self.store)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["products"] = Product.objects.filter(store=self.store)
+        return context
 
 
 class StoreProduct(ListView):
     model = Product
-    template_name = 'products/product_list.html'
-    context_object_name = 'products'
-
-    def get_queryset(self):
-        self.store = get_object_or_404(Store, slug=self.kwargs['slug'])
-        return Product.objects.filter(store=self.store)
-
-    def get_context_data(self, **kwargs):
-        context = super(StoreProduct, self).get_context_data(**kwargs)
-        context["cat_bar"] = Category.objects.all()
-        context["store_products"] = self.store
-        return context
-
-
-class ProductCategory(ListView):
-    model = Product
-    template_name = 'products/categories.html'
-    context_object_name = 'pro_list'
+    template_name = 'products/cat_product_list.html'
+    context_object_name = 'cat_products'
 
     def get_queryset(self):
         self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
@@ -36,10 +35,10 @@ class ProductCategory(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["cat_bar"] = Category.objects.all()
+        context["categories"] = Category.objects.filter(store=self.category.store)
         return context
 
 
 class ProductDetail(DetailView):
     model = Product
-    context_object_name = "picture"
+    context_object_name = "product"
